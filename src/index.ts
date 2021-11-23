097209c919a50419485082c90020fb8fbaa52740
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-23 19:36:58 -0300

    [Configuration]-Add packages

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors'
+import {createConnection} from 'typeorm'
+
+
+const app = express();
+createConnection();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// routes
+app.use(userRoutes);
+
+app.listen(3000);
+console.log('Server on port', 3000);
