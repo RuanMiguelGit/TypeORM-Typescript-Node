9fdb0517a015432aa278ee16cbad130aaa722bc4
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-23 19:41:11 -0300

    [CONFIGURATION] Add router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
 import {createConnection} from 'typeorm'
+import userRoutes from '../src/routes/user'
+
 
 
 const app = express();
@@ -13,6 +15,6 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // routes
-
+app.use(userRoutes)
 app.listen(3000);
 console.log('Server on port', 3000);
